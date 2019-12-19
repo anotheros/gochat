5cefd8726b8e14f0ce6dfa73d519c3a718424af6
anotheros <1773066+anotheros@users.noreply.github.com>
2019-12-20 00:38:39 +0800

    websocket 推送消息

--- connect/connect.go
+++ connect/connect.go
@@ -42,9 +42,7 @@ func (c *Connect) Run() {
 			RoutineSize:   connectConfig.ConnectBucket.RoutineSize,
 		})
 	}
-
-	//var dispatcher = NewDispatcher(MaxWorker)
-	//dispatcher.Run()
+	
 	operator := new(DefaultOperator)
 	DefaultServer = NewServer(Buckets, operator, ServerOptions{
 		WriteWait:       10 * time.Second,
